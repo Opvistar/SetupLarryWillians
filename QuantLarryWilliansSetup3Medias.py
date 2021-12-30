fa1dc5b78dc9b2c8aac7bb584574da08d70b5736
Aprendiz de Feiticeiro <71798535+Opvistar@users.noreply.github.com>
2021-12-30 13:50:39 -0300

    Update QuantLarryWilliansSetup3Medias.py

--- QuantLarryWilliansSetup3Medias.py
+++ QuantLarryWilliansSetup3Medias.py
@@ -2,24 +2,19 @@
 # Autor: @opvistar (Twitter) - dez.2021                                       #
 # SETUP LARRY WILLIANS 3 MÉDIAS MÓVEIS                                        #
 # DESCRIÇÃO DO SETUP : https://www.youtube.com/watch?v=QXzkrR-dX1E            #
-# VERSÃO: 1.2.0 (26.12.21)                                                    #
-# HISTORICO ALTERACOES                                                        #
-# 1.1.0 - adicionei nova feature, agora salva CSV para visualizar no Excel    #
-# entretanto o cabeçalho tem que ser inserido manualmente.                    #
-# separador é ";"                                                             #
-# 1.2.0 - (a) agora verifica se abriu em gap acima ou abaixo das SMA3min#     #
-# e SMA3max# # tomando cuidado pra comprar na abertura se preço não cruza     #
-# essas médias (b) desabiilitei o código da operação de VENDA                 # 
-#                                                                             #
-# 1.2.1 - (a) bug de comparação da SMA21 na venda, ">" para "<"               #
-#         l_sma21[x-1] < l_sma21[x-2]                                         #
-#         (b) agora arquivos identificam se é compra/venda                    #
-#==============================================================================
+# VERSÃO: 1.2.0 (30.12.21 - FINAL)                                            #
+# AO EXECUTAR O SCRIPT CRIE ANTES DOIS SUBDIRETÓRIOS                          #
+# ../ENTRADA E ../SAIDA                                                       #
+# NÃO, O SCRIPT NÃO CRIA SOZINHO...                                           #
+# NÃO ESQUECE DE POPULAR O DIRETÓRIO DE ENTRADA COM OS ARQUIVOS DO            #
+# TRYD, E TAMBÉM DE POPULAR A VARIÁVEL LISTA_B3, É ESSA VARIÁVEL QUE VAI      #
+# DETERMINAR QUE ATIVOS ELE VAI LER DO DIRETÓRIO DE ENTRADA                   #
+##############################################################################
 
 import datetime
 import pandas as pd
 import Migracao_Tryd_para_Python  as migratryd
-
+import os as os
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -30,22 +25,27 @@ import csv
 
 ###################################################################################################################################
 #                                                                                                                                 #
-######                                                     INICIO DO PROGRAMA                                                ######
+#                                                   INICIO DO PROGRAMA                                                            #
 #                                                                                                                                 #
 ###################################################################################################################################
 
    
 
 #############     PARÂMETROS DE CONFIGURACAO     ################
-LISTA_B3 = {"WIN"}     # lista de ativos que vou pegar os dados (TICKER DA B3!!)
-PERIODOS_SMA = 21
-NUMERO_DE_DIAS = 2000     # qtdade dias que vai pegar do histórico do Tryd
+
+LISTA_B3 = {"EGIE3"}     # lista de ativos que vou pegar os dados (TICKER DA B3!!)
+MEDIA_LONGA = 21           # parametro média longa
+NUMERO_DE_DIAS = 5000      # qtdade dias que vai pegar do histórico do Tryd
 DESABILITA_VENDA = False   # somente operacões de compra = True
+MEDIAS_CURTAS = 3          # parametro médias curtas
+DIR_ENTRADA = "ENTRADA"    #
+DIR_SAIDA = "SAIDA"        #
+
 ###############################################################  
 
 
 # converte arquivo do Tryd para datframe Python 
-migratryd.ConverteArquivosTryd(LISTA_B3,NUMERO_DE_DIAS)
+migratryd.ConverteArquivosTryd(LISTA_B3,NUMERO_DE_DIAS,DIR_ENTRADA,DIR_SAIDA)
 
 dict_ativos_precos = {}
 
@@ -58,16 +58,11 @@ SAIDA    = 3
 RESULTADO = 4
 ACUMULADO = 5
 TIPO      = 6
-
-if( DESABILITA_VENDA ):
-    texto ="_Compra_"
-else:
-    texto = "_Compra_Venda_"
-
-
+ 
 for ativo in LISTA_B3:
  
-     file_name_py  =  ativo + "_python.csv"
+     file_name_py = os.path.join(os.getcwd(), DIR_SAIDA,ativo + "_Python.csv" )
+
      lSucesso = True
      ativo_python = pd.DataFrame(columns = ["Data","Abertura", "Fechamento","Maxima","Minima"])    
      
@@ -90,7 +85,9 @@ print('\n')
 #######################  INICIO DO ALGORITMO #####################
 ##################################################################
 # itero sobre lista de ativos
-nome_arquivo = "Resultado_" + texto + datetime.datetime.now().strftime('%Y_%d_%m_%H_%M_%S') + ".txt"
+nome_arquivo = os.path.join(os.getcwd(), DIR_SAIDA,"Logs_LWClassico_" 
+                            + datetime.datetime.now().strftime('%Y_%d_%m_%H_%M_%S') + ".txt" )
+
 file_relat = open( nome_arquivo, "a")  # DEBUG
 if dict_ativos_precos:
 
@@ -100,9 +97,9 @@ if dict_ativos_precos:
         # inverto as linhas (de forma que linha 0 fique por ultimo e ultima linha por primeiro)   
         vx_preco  = vx_preco[::-1]
         
-        sma21 = vx_preco['Fechamento'].rolling(window = PERIODOS_SMA).mean()
-        sma3_max = vx_preco['Maxima'].rolling(window = 3 ).mean()
-        sma3_min = vx_preco['Minima'].rolling(window = 3 ).mean()
+        sma21 = vx_preco['Fechamento'].rolling(window = MEDIA_LONGA).mean()
+        sma3_max = vx_preco['Maxima'].rolling(window = MEDIAS_CURTAS ).mean()
+        sma3_min = vx_preco['Minima'].rolling(window = MEDIAS_CURTAS ).mean()
         
         # comeca do ultimo para o 1o
         # só encerro a operação no outro dia, pois não sei se abriu e bateu na SMA3_max primeiro ou na SMA3_min primeiro, só 
@@ -118,7 +115,7 @@ if dict_ativos_precos:
         l_preco_abe  = vx_preco['Abertura'].tolist()        
         l_preco_Data = vx_preco['Data'].tolist()
 
-        for x in range(PERIODOS_SMA + 1,len(l_preco_Data), 1):
+        for x in range(MEDIA_LONGA + 1,len(l_preco_Data), 1):
              
             if not historico:
                 acumulado = 0
@@ -130,64 +127,80 @@ if dict_ativos_precos:
                 saida = elemento_historico[SAIDA]
 
             # se for diferente de -1, não tem operação aberta...
+            premissa1 = l_preco_min[x] <= l_sma3_min[x] # Minima bateu SMA3min?
+            premissa2 = l_sma21[x-1] > l_sma21[x-2]     # SMA21 mais recente MAIOR que anterior
+            premissa3 = l_sma21[x-1] < l_sma21[x-2]     # SMA21 mais recente MENOR que anterior
+            premissa4 = l_preco_max[x] >= l_sma3_max[x] # Máxima bateu SMA3max?
+           
+            # NENHUMA OPERACAO EM ABERTO!                              
             if( saida != -1):
 
-                ## VERIFICO SE A MEDIA MOVEL SMA21 TA SUBINDO...
-                if( (l_preco_min[x] < l_sma3_min[x]) and  l_sma21[x-1] > l_sma21[x-2] ): # SMA21 mais recente maior que anterior
+                #
+                if( premissa1 and premissa2 ):  
 
-                    # tenho que verificar se abriu em gap de baixa, as vezes abriu em gap e a máxima do dia ainda está abaixo da SMA3min.
+                    premissa1 = l_preco_abe[x] < l_sma3_min[x] # se já abriu abaixo da SMA3max, já compro na abertura
                     
-                    # se já abriu abaixo da SMA3_min, já compro na abertura
-                    if( l_preco_abe[x] < l_sma3_min[x]): 
+                    if( premissa1 ): 
                        historico.append( [l_preco_Data[x],"",l_preco_abe[x],-1,0,acumulado,"COMPRA"] )
                     else:
-                    # neste caso efetua a compra do ativo quando cruza SMA3min 
+                         # neste caso efetua a compra do ativo quando cruza SMA3min 
                         historico.append( [l_preco_Data[x],"",l_sma3_min[x],-1,0,acumulado,"COMPRA"] )
-
-                # ## VERIFICO SE A MEDIA MOVEL SMA21 TA DESCENDO...
-                elif( (l_preco_max[x] > l_sma3_max[x]) and  l_sma21[x-1] < l_sma21[x-2] and not DESABILITA_VENDA): # SMA21 mais recente menor que anterior
-                    # efetua a venda do ativo                      
-                    if( l_preco_abe[x] > l_sma3_max[x]): 
+                 
+                elif( premissa3 and  premissa4 and not DESABILITA_VENDA): # SMA21 mais recente menor que anterior
+                    
+                    premissa1 = l_preco_abe[x] > l_sma3_max[x] # se já abriu acima da SMA3max, já compro na abertura
+                     
+                    if( premissa1 ): 
                        # se abriu em gap de alta, ja vendo na abertura
                        historico.append( [l_preco_Data[x],"",l_preco_abe[x],-1,0,acumulado,"VENDA"] )
                     else:                    
                         historico.append( [l_preco_Data[x],"",l_sma3_max[x],-1,0,acumulado,"VENDA"] ) 
                     
-            # operacao em aberto, verifica se pode encerrar                    
+            # OPERACAO EM ABERTO! VERIFICA SE PODE ENCERRAR...                  
             else:    
+                
                 # agora verifico se algum dos criterios foram atendidos...
-                elemento_historico = historico[len(historico)-1]    
-                                 
-                if( l_preco_max[x] >= l_sma3_max[x] and elemento_historico[TIPO] == "COMPRA"):
+                elemento_historico = historico[len(historico)-1]  
+                                           
+                                    
+                # só verifica se não bateu o loss na abertura
+                premissa1 = l_preco_max[x] >= l_sma3_max[x]
+                premissa2 = l_preco_min[x] <= l_sma3_min[x]
+                
+                if( premissa1 and elemento_historico[TIPO] == "COMPRA"):
                      # encerra a operaçã0 de compra
-
                     # tenho que verificar se abriu em gap de alta, as vezes abriu em gap e a mínima do dia ainda está acima do SMA3max.
-                   
-                    # se abriu com gap, já zero na abertura
-                    if( l_preco_abe[x] > l_sma3_max[x]): 
+                    # 
+                    premissa1 = l_preco_abe[x] > l_sma3_max[x] # se abriu com gap de ALTA, já zero na abertura
+                    
+                    if( premissa1 ): 
                         elemento_historico[SAIDA] = l_preco_abe[x]
                     else:
                         # senao zera ao tocar na SMA3max                        
                         elemento_historico[SAIDA] = l_sma3_max[x]
                         
-                    elemento_historico[DATA_SAI] = l_preco_Data[x]
+                    elemento_historico[DATA_SAI]  = l_preco_Data[x]
                     elemento_historico[RESULTADO] = elemento_historico[SAIDA] - elemento_historico[ENTRADA]
                     elemento_historico[ACUMULADO] = elemento_historico[ACUMULADO] + elemento_historico[RESULTADO]
-                    historico[len(historico)-1] = elemento_historico
+                    historico[len(historico)-1]   = elemento_historico
+
+                   
+                elif( premissa2  and elemento_historico[TIPO] == "VENDA" and not DESABILITA_VENDA):
+
+                    premissa1 = l_preco_abe[x] < l_sma3_min[x] # se abriu com gap de BAIXA, já zero na abertura
                     
-                elif( l_preco_min[x] <= l_sma3_min[x] and elemento_historico[TIPO] == "VENDA" and not DESABILITA_VENDA):
-                    #encerra a operação de venda se abriu abaixo da SMA3max    
-                    if(l_preco_abe[x] < l_sma3_min[x]):  
+                    if( premissa1 ):  
                         # se abriu com gap, já zero na abertura
                         elemento_historico[SAIDA] = l_preco_abe[x]
                     else:              
                         # senao zera ao tocar na SMA3min
                         elemento_historico[SAIDA] = l_sma3_min[x]
                         
-                    elemento_historico[DATA_SAI] = l_preco_Data[x]
+                    elemento_historico[DATA_SAI]  = l_preco_Data[x]
                     elemento_historico[RESULTADO] = elemento_historico[ENTRADA] - elemento_historico[SAIDA]
                     elemento_historico[ACUMULADO] = elemento_historico[ACUMULADO] + elemento_historico[RESULTADO]
-                    historico[len(historico)-1] = elemento_historico                    
+                    historico[len(historico)-1]   = elemento_historico    
+                
 
 
         print('\n',file=file_relat)        
@@ -206,8 +219,10 @@ if dict_ativos_precos:
             elemento_historico[RESULTADO] = str(round(elemento_historico[RESULTADO],2)).replace('.',',')
             elemento_historico[ACUMULADO] = str(round(elemento_historico[ACUMULADO],2)).replace('.',',')
             historico_csv.append(elemento_historico)
-            
-        with open("Planilha_"+ texto + kx_ativo +"_"+ datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S') + '.csv', 'w', newline='\n') as csvfile:
+
+        nome_arquivo = os.path.join(os.getcwd(), DIR_SAIDA, 
+                                    "Relatorio_LWClassico_" + kx_ativo + "_" + datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S') + ".csv" )  
+        with open(nome_arquivo, 'w', newline='\n') as csvfile:
             writer = csv.writer(csvfile)
             csvwriter = csv.writer(csvfile,delimiter =';')
             # 'Numerador','Denominador','Preço_NUM','Preço_DEN','Ratio_Ult' 'Ratio Inst' 'Ratio_medio','Ratio_2D_minus','Ratio_2D_plus','Ratio_3D_minus','Ratio_3D_plus','Ratio_4D_minus','Ratio_4D_plus' 
@@ -217,5 +232,6 @@ if dict_ativos_precos:
         
 print("Backtest conluído, verifique o arquivo " + nome_arquivo)    
 file_relat.close()    
+
 
 
